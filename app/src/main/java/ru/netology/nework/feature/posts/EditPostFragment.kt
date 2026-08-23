c33ee6202e2fd74ac263992b12e92e2f14df29e1
Aleksandr Karkhunen <ajlvatanabe@gmail.com>
2026-08-23 23:17:24 +0300

    Дизайн

---
+++ app/src/main/java/ru/netology/nework/feature/posts/EditPostFragment.kt
@@ -0,0 +1,28 @@
+package ru.netology.nework.feature.posts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.netology.nework.databinding.FragmentEditPostBinding
+
+class EditPostFragment : Fragment() {
+
+    private var _binding: FragmentEditPostBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEditPostBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
